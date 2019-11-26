72e7c74a04823c762d82966d1275a47baff1a02d
Darkcyde <56904224+Darkcyde@users.noreply.github.com>
2019-11-26 08:36:43 -0600

    Redid project

--- CSP.py
+++ CSP.py
@@ -1,19 +1,69 @@
 import random
 
+# Fight function to ask for spell and attack enemy
+def Fight():
+    enemyHealth = 10
+    
+
+    while enemyHealth > 0:
+        
+        
+        spell = input()
+        if (spell == "Fire" or spell == "fire"):
+            damage = random.randint(1, 5)
+            enemyHealth = enemyHealth - damage
+            print("You did", damage, "damage")
+            print("the enemy has", enemyHealth, "health")
+        else:
+            print("Invalid spell!")
+
 print ('Welcome to my Game!')
 print ('Would you like to start')
 
 userInput = input()
 
-if (userInput == "Yes" or userInput == "yes"):
+if (userInput == "Yes" or userInput == "yes" or userInput == "YEs"):
     print("Then let's get started, whats your Username?")
     name = input()
     print("Hello, " + name)
     print("Quick, a goblin has appeared! What spell will you use")
-    print("Choose Fire or Ice")
-    spell = input()
-    if (spell == "Fire"):
-        damage = random.randint(1, 5)
-        print("You did " + damage + "damage")
+    
+    enemyHealth = 10
+    
+
+    while enemyHealth > 0:
+        
+        print("Choose Fire or Ice")
+        
+        spell = input()
+        if (spell == "Fire" or spell == "fire"):
+            damage = random.randint(3, 5)
+            enemyHealth = enemyHealth - damage
+            print("You did", damage, "damage")
+            print("the enemy has", enemyHealth, "health")
+            
+        elif (spell == "Ice" or spell == "ice"):
+            damage = random.randint(1, 7)
+            enemyHealth = enemyHealth - damage
+            print("You did", damage, "damage")
+            print("the enemy has", enemyHealth, "health")
+            
+        else:
+            print("Invalid spell!")
+        
+    if (enemyHealth <= 0):
+        print("You killed the witch!")
+        print("Another one appears!")
+        enemyHealth = 15
+        Fight()
+        
+    if (enemyHealth <= 0):
+        print("You killed the goblin!")
+        print("Another one appears!")
+        enemyHealth = 30
+        Fight()
+
 else:
     print("Error, Wrong answer")
+    
+print("Great job, you got a score of", score, "and beat my score")
